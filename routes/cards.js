529312d21885a77e55bab4f2d68d822d098e1c1e
Rim Sungatovich <rsungatovichsocial@gmail.com>
2020-07-24 23:20:46 +0300

    fixed routers logic, added error messages

--- routes/cards.js
+++ routes/cards.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const filepath = path.join(__dirname, '../data/cards.json');
 
 router.get('/', (req, res) => {
-  const reader = fs.createReadStream(filepath, { encoding: 'utf8' });
-  res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-  reader.pipe(res);
+  fs.readFile(filepath, { encoding: 'utf8' })
+    .then((data) => {
+      const cardsData = JSON.parse(data);
+      res.send(cardsData);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,45 @@
 /* eslint-disable no-underscore-dangle */
 const router = require('express').Router();
 const path = require('path');
-const fs = require('fs');
-const users = require('../data/users.json');
+const fs = require('fs').promises;
 
 const filepath = path.join(__dirname, '../data/users.json');
 
 const sendUsers = (req, res) => {
-  const reader = fs.createReadStream(filepath, { encoding: 'utf8' });
-  res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-  reader.pipe(res);
+  fs.readFile(filepath, { encoding: 'utf8' })
+    .then((data) => {
+      const usersData = JSON.parse(data);
+      res.send(usersData);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 };
 
 const checkUser = (req, res, next) => {
-  if (!users.find((user) => user._id === req.params.id)) {
-    res.status(404).send({ message: 'Нет пользователя с таким id' });
-    return;
-  }
-
-  next();
+  fs.readFile(filepath, { encoding: 'utf8' })
+    .then((data) => {
+      const usersData = JSON.parse(data);
+      if (!usersData.find((user) => user._id === req.params.id)) {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      next();
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 };
 
 const sendUser = (req, res) => {
-  res.send(users.find((user) => user._id === req.params.id));
+  fs.readFile(filepath, { encoding: 'utf8' })
+    .then((data) => {
+      const usersData = JSON.parse(data);
+      res.send(usersData.find((user) => user._id === req.params.id));
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 };
 
 router.get('/', sendUsers);
